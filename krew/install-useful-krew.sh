92cb6b91fe820472423f08f3bb4959b0998ac82f
Phil Huang <hphil@vmware.com>
2022-01-28 00:38:34 +0800

    Update
    Signed-off-by: Phil Huang <hphil@vmware.com>

---
+++ krew/install-useful-krew.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+kubectl krew update
+kubectl krew upgrade
+kubectl krew install ns
+kubectl krew install ctx
+kubectl krew install get-all
+kubectl krew install images
+kubectl krew install whoami
+kubectl krew install rbac-lookup
+kubectl krew install grep
+kubectl krew install iexec
+kubectl krew install tree
+kubectl krew install view-utilization
+kubectl krew install tail
+kubectl krew install access-matrix
+kubectl krew install doctor
+kubectl krew install resource-capacity
+kubectl krew install df-pv
+kubectl krew install view-secret
--- prometheus/README.md
+++ prometheus/README.md
@@ -1,1 +1,4 @@
 PACKAGE-VERSION: 2.27.0+vmware.1-tkg.1
+
+
+https://docs.vmware.com/en/VMware-Tanzu-Kubernetes-Grid/1.4/vmware-tanzu-kubernetes-grid-14/GUID-packages-prometheus.html#review-configuration-parameters-9
